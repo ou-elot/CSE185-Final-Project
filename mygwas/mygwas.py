72adaf1965942d7d3d4dd0c9a0b1cdeaa9e54b11
ou-elot <146787722+ou-elot@users.noreply.github.com>
2024-05-13 21:06:46 -0700

    Update mygwas.py

--- mygwas/mygwas.py
+++ mygwas/mygwas.py
@@ -1,57 +1,7 @@
 import numpy as np
-
-def SimulateGenotypes(maf, N):
-    """
-    Simulate genotypes for N samples with minor 
-    allele frequency maf (assuming HWE)
-    
-    Parameters
-    ----------
-    maf : float
-        Minor allele frequency of the SNP
-    N : int
-        Sample size (number of people)
-        
-    Returns
-    -------
-    gts : np.array of floats
-        Genotypes (scaled to have mean 0, variance 1)
-        of each person
-    """
-    gts = []
-    for i in range(N):
-        gt = sum(random.random() < maf)+sum(random.random() < maf)
-        gts.append(gt)
-    # Technical note: scale to have mean 0 var 1
-    gts = np.array(gts)
-    gts = (gts-np.mean(gts))/np.sqrt(np.var(gts))
-    return gts
-
-def SimulatePhenotype(gts, Beta):
-    """
-    Simulate phenotypes under a linear model Y=beta*X+error
-    
-    Parameters
-    ----------
-    gts : np.array of floats
-        Genotypes of each person
-    Beta : float
-        Effect size
-        
-    Returns
-    -------
-    pts : np.array och personf floats
-        Simulated phenotype value of each person
-    """
-    if Beta<-1 or Beta>1:
-        print("Error: Beta should be between -1 and 1")
-        return [None]*len(pts)
-    pts = Beta*gts + np.random.normal(0, np.sqrt(1-Beta**2), size=len(gts))
-    return pts
-
 import statsmodels.api as sm
 
-def Linreg(gts, pts):
+def LinearRegression(geno, pheno):
     """
     Perform a GWAS between the genotypes and phenotypes
     
@@ -76,3 +26,16 @@ def Linreg(gts, pts):
     beta = results.params[1]
     pval = results.pvalues[1]
     return beta, pval
+
+def getGenotype(vcf_file):
+    pd.read_csv(vcf_file, comment="#", sep="\t", names=column_name)
+    column_name = []
+    with open(vcf_file, 'r') as file:
+        for line in file:
+            if line.startswith('#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT'):
+                column_name = line.split('\t')
+                column_name[0] = column_name[0][1:]  # remove #
+                column_name[-1] = column_name[-1][:-1]  # remove \n
+        
+        
+    pd.read_csv(vcf_file, comment="#", sep="\t", names=column_name)
