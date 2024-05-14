3c79a6169b3c04c5cc58aa460f215086b400f67c
ou-elot <146787722+ou-elot@users.noreply.github.com>
2024-05-13 20:48:55 -0700

    Update mygwas.py

--- mygwas/mygwas.py
+++ mygwas/mygwas.py
@@ -1,18 +1,78 @@
 import numpy as np
-import pandas as pd
+
+def SimulateGenotypes(maf, N):
+    """
+    Simulate genotypes for N samples with minor 
+    allele frequency maf (assuming HWE)
+    
+    Parameters
+    ----------
+    maf : float
+        Minor allele frequency of the SNP
+    N : int
+        Sample size (number of people)
+        
+    Returns
+    -------
+    gts : np.array of floats
+        Genotypes (scaled to have mean 0, variance 1)
+        of each person
+    """
+    gts = []
+    for i in range(N):
+        gt = sum(random.random() < maf)+sum(random.random() < maf)
+        gts.append(gt)
+    # Technical note: scale to have mean 0 var 1
+    gts = np.array(gts)
+    gts = (gts-np.mean(gts))/np.sqrt(np.var(gts))
+    return gts
+
+def SimulatePhenotype(gts, Beta):
+    """
+    Simulate phenotypes under a linear model Y=beta*X+error
+    
+    Parameters
+    ----------
+    gts : np.array of floats
+        Genotypes of each person
+    Beta : float
+        Effect size
+        
+    Returns
+    -------
+    pts : np.array och personf floats
+        Simulated phenotype value of each person
+    """
+    if Beta<-1 or Beta>1:
+        print("Error: Beta should be between -1 and 1")
+        return [None]*len(pts)
+    pts = Beta*gts + np.random.normal(0, np.sqrt(1-Beta**2), size=len(gts))
+    return pts
+
 import statsmodels.api as sm
-import matplotlib.pyplot as plt
 
-def get_vcf(vcf_file):
-  with gzip.open(vcf_path, "rt") as file:
-          for line in file:
-            if line.startswith("#CHROM"):
-                  vcf = [x for x in line.split('\t')]
-                  break
-    file.close()
-    return vcf
-
-names = get_vcf('file.vcf.gz')
-vcf = pd.read_csv('file.vcf.gz', compression='gzip', comment='#', chunksize=10000, delim_whitespace=True, header=None, names=names)
-
-wget -O - "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/release/20190312_biallelic_SNV_and_INDEL/ALL.chr14.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz" >> ~/Final/test.vcf.gz
+def Linreg(gts, pts):
+    """
+    Perform a GWAS between the genotypes and phenotypes
+    
+    Parameters
+    ----------
+    gts : np.array of floats
+        Genotypes (scaled to have mean 0, variance 1)
+        of each person
+    pts : np.array och personf floats
+        Simulated phenotype value of each person
+        
+    Returns
+    -------
+    beta : float
+        Estimated effect size
+    pval : float
+        P-value
+    """
+    X = sm.add_constant(gts)
+    model = sm.OLS(pts, X)
+    results = model.fit()
+    beta = results.params[1]
+    pval = results.pvalues[1]
+    return beta, pval
