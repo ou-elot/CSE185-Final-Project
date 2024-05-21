659cf4fd35da5bf7a36ffea9ec790ef269d85b94
ou-elot <146787722+ou-elot@users.noreply.github.com>
2024-05-20 20:22:51 -0700

    Update mygwas.py

--- mygwas/mygwas.py
+++ mygwas/mygwas.py
@@ -1,7 +1,30 @@
+import io
+import os
+import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def LinearRegression(geno, pheno):
+def genotype(genotype_path):
+    geno= pd.read_csv(genotype_path, comment="#", sep="\t", header = None)
+    y=-1
+    for index, row in geno.iterrows():
+        y=y+1
+        for i in range (9, len(geno.axes[1]) ):
+            gt = row[i]
+            alleles = gt.split("|") 
+            alleles = [int(item) for item in alleles]
+            final_genotype = sum(alleles)
+            geno.at[y, i] = final_genotype 
+    return geno
+
+
+def getPhenotype(phenotype_path):
+    phen = pd.read_csv(phenotype_path, sep='\t', header =None)
+    return phen
+
+def Linreg(gts, pts):
     """
     Perform a GWAS between the genotypes and phenotypes
     
@@ -27,21 +50,20 @@ def LinearRegression(geno, pheno):
     pval = results.pvalues[1]
     return beta, pval
 
-import io
-import os
-import pandas as pd
+def gwas (geno_file, pheno_file):
+    a = []
+    genoCopy = genotype('gwas_test.vcf')
+    geno = genotype('gwas_test.vcf')
+    geno.drop(geno.columns[[0,1,2,3,4,5,6,7,8]], axis=1, inplace=True)
 
-def read_vcf(path):
-    with open(path, 'r') as f:
-        lines = [l for l in f if not l.startswith('##')]
-    return pd.read_csv(
-        io.StringIO(''.join(lines)),
-        dtype={'#CHROM': str, 'POS': int, 'ID': str, 'REF': str, 'ALT': str,
-               'QUAL': str, 'FILTER': str, 'INFO': str},
-        sep='\t'
-    ).rename(columns={'#CHROM': 'CHROM'})
 
-def getPhenotype(phenotype_path):
-    phen = pd.read_csv(phenotype_path, sep='\t', header =None)
-    return phen 
-
+    pheno = getPhenotype('lab3_gwas.phen')
+    pts = pheno[2]
+    for index, row in geno.iterrows():
+        gts = geno.iloc[index].to_numpy()
+        gts = gts.astype(np.float)
+        beta, pval = Linreg(gts, pts);
+        b = [genoCopy[2][index], pval, beta]
+        a.append(b)
+    data = pd.DataFrame(a, columns = ['Gene Name', 'p-value', 'Beta value'])
+    return data
