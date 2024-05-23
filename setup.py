bf2d9de15a4b13524aadacc1ff5268738c46f73c
ou-elot <146787722+ou-elot@users.noreply.github.com>
2024-05-22 19:28:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,6 @@ import os
 from setuptools import setup, find_packages
 from gwas_tools.__version__ import __version__
 
-# version-keeping code based on pybedtools
-curdir = os.path.abspath(os.path.dirname(__file__))
-MAJ = 0
-MIN = 0
-REV = 0
-VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
-with open(os.path.join(curdir, 'mygwas/version.py'), 'w') as fout:
-        fout.write(
-            "\n".join(["",
-                       "# THIS FILE IS GENERATED FROM SETUP.PY",
-                       "version = '{version}'",
-                       "__version__ = version"]).format(version=VERSION)
-        )
-
-
 setup(
     name='mygwas',
     version=VERSION,
