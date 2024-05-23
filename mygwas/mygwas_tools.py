37cb6c68e606a10e6cdbd1ce5927e592df091773
ou-elot <146787722+ou-elot@users.noreply.github.com>
2024-05-22 18:17:47 -0700

    Update mygwas_tools.py

--- mygwas/mygwas_tools.py
+++ mygwas/mygwas_tools.py
@@ -5,6 +5,7 @@ import numpy as np
 import statsmodels.api as sm
 import seaborn as sns
 import matplotlib.pyplot as plt
+from qqman import qqman
 
 def genotype(genotype_path):
     geno= pd.read_csv(genotype_path, comment="#", sep="\t", header = None)
@@ -56,15 +57,21 @@ def gwas (geno_file, pheno_file, output):
     geno = genotype('gwas_test.vcf')
     geno.drop(geno.columns[[0,1,2,3,4,5,6,7,8]], axis=1, inplace=True)
 
-
     pheno = getPhenotype('lab3_gwas.phen')
     pts = pheno[2]
     for index, row in geno.iterrows():
         gts = geno.iloc[index].to_numpy()
         gts = gts.astype(np.float)
         beta, pval = Linreg(gts, pts);
-        b = [genoCopy[2][index], pval, beta]
+        b = [genoCopy[0][index], genoCopy[2][index], genoCopy[1][index], pval, beta]
         a.append(b)
-    data = pd.DataFrame(a, columns = ['Gene Name', 'p-value', 'Beta value'])
+    data = pd.DataFrame(a, columns = ['CHR', 'SNP', 'BP', 'P', 'BETA'])
     data.to_csv(output+'beta_pval.txt', sep='\t', index=False)
     return data
+
+def graphs (gwas_data, output):
+    data = pd.read_csv(gwas_data), delim_whitespace=True)
+    fig, (ax0, ax1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [2, 1]})
+    fig.set_size_inches((15, 5))
+    qqman.manhattan(data, ax=ax0, out= output+"Manhattan.png")
+    qqman.qqplot(data, ax=ax1, out=output+"qq.png")
