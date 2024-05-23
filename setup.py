c7f036445f2401ddbc5544258aede9203dcafa52
ou-elot <146787722+ou-elot@users.noreply.github.com>
2024-05-22 18:50:44 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 setup(
   name = 'gwas',
-  version = VERSION,
+  version = 0.1,
   description= 'CSE 185 S24 Final Project Gwas',
   authors = 'Elliott Ou, Lenny Lei, Audria Montalvo',
   author_emails = 'elou@ucsd.edu, wlei@ucsd.edu, ansaravi@ucsd.edu',
+  packages = find_packages(),
   entry_points = {
     "console_scripts": [
-      "mygwas = mygwas.mygwas:main"
+      "mygwas=mygwas.mygwas:main"
     ],
   },
-  install_requires=[
-        'pandas',
-        'numpy',
-        'matplotlib',
-        'statsmodels',
-  ],
 )
