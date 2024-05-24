e7bf7bb986ed14f4a0fd222a40a1e11a46431144
Hybryl <129354339+Hybryl@users.noreply.github.com>
2024-05-23 18:52:27 -0700

    mygwas_tools.py
    Added comment

--- mygwas/mygwas_tools.py
+++ mygwas/mygwas_tools.py
@@ -9,6 +9,21 @@ import matplotlib.pyplot as plt
 from qqman import qqman
 
 def genotype(genotype_path):
+    """
+    This function processes a genotype file and converts genotype data from a 
+    string representation of alleles to their numeric sum. It reads the input
+    file, iterates over the data, and updates the genotype values.
+
+    Parameters
+    ----------
+    genotype_path (str): The path to the genotype file. The file is expected 
+                         to be tab-separated and may contain comments starting
+                         with '#'.
+    Returns
+    -------
+    pd.DataFrame: A pandas DataFrame with updated genotype values, where each 
+                  genotype is the sum of its alleles.
+    """
     geno= pd.read_csv(genotype_path, comment="#", sep="\t", header = None)
     y=-1
     for index, row in geno.iterrows():
@@ -53,6 +68,24 @@ def Linreg(gts, pts):
     return beta, pval
 
 def gwas (geno_file, pheno_file, output):
+    """
+     This function performs a Genome-Wide Association Study (GWAS) by 
+    analyzing genotype and phenotype data. It reads the genotype and 
+    phenotype files, performs linear regression for each SNP, and 
+    outputs the results.
+
+    Parameters
+    ----------
+    geno_file (str): The path to the genotype file (expected in VCF format).
+    pheno_file (str): The path to the phenotype file.
+    output (str): The path for the output file to save the GWAS results.
+
+    Returns
+    -------
+    pd.DataFrame: A pandas DataFrame containing the GWAS results, with columns
+                  for chromosome, SNP ID, base pair position, p-value, and 
+                  regression coefficient (beta).
+    """
     a = []
     genoCopy = genotype('gwas_test.vcf')
     geno = genotype('gwas_test.vcf')
@@ -70,6 +103,19 @@ def gwas (geno_file, pheno_file, output):
     return data
 
 def graphs (gwas_data, output):
+    """
+    This function creates visualizations for Genome-Wide Association Study (GWAS) results.
+    It reads the GWAS results from a file and generates a Manhattan plot and a QQ plot using the
+    `qqman` library, then saves these plots to specified output files.
+
+    Parameters
+    ----------
+    gwas_data (str): The path to the file containing GWAS results. The file is expected to be
+                     in tab-separated format with columns for chromosome (CHR), SNP, base pair position (BP),
+                     p-value (P), and beta coefficient (BETA).
+    output (str): The base path for the output files to save the plots. The function will save
+                  two plots: a Manhattan plot and a QQ plot, with filenames derived from this base path.
+    """
     gwas = pd.read_csv((gwas_data), sep='\t')
     fig, (ax0, ax1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [2, 1]})
     fig.set_size_inches((15, 5))
