d01772e560395dac5d0db1fe8417675e275fe4c8
ou-elot <146787722+ou-elot@users.noreply.github.com>
2024-05-30 16:04:07 -0700

    Update mygwas.py

--- mygwas/mygwas.py
+++ mygwas/mygwas.py
@@ -16,12 +16,39 @@ def main():
     --pheno (str): Path to the text file containing phenotype data.
     """
     parser = argparse.ArgumentParser(
-        prog = "mygwas",
-        description = "Command Line Script to perform gwas"
+        #prog = "mygwas",
+        #description = "Command Line Script to perform gwas"
+        prog=f"{BLUE}mygwas",
+        description=(
+            f"{PINK}============================================================\n"
+            f"                 GWAS Command Line Tool \n"
+            "============================================================\n"
+            f"This tool performs {CYAN}Genome-Wide Association Studies (GWAS){PINK}.\n"         
+            "It processes genotype and phenotype data, conducts linear regression \n"
+            "to obtain statistical associations, and generates visualizations such \n" 
+            "as Manhattan plots and QQ plots.\n"
+            "------------------------------------------------------------\n"
+            f"Example usage:\n"
+            f"{BLUE}mygwas --geno path/to/genotype.vcf --pheno path/to/phenotype.phen {PINK}\n"
+            "============================================================"
+        ),
+        formatter_class=argparse.RawTextHelpFormatter
     )
     #Input
-    parser.add_argument("--geno", help="vcf file of a genome that contains genotype", type = str)
-    parser.add_argument("--pheno", help="txt file that contains phenotypes of a genome", type = str)
+    #parser.add_argument("--geno", help="vcf file of a genome that contains genotype", type = str)
+    #parser.add_argument("--pheno", help="txt file that contains phenotypes of a genome", type = str)
+    parser.add_argument(
+        "--geno", 
+        help=f"{BLUE}VCF file that contains genotype data{PINK}", 
+        type = str,
+        required = True
+    )
+    parser.add_argument(
+        "--pheno", 
+        help = f"{BLUE}PHENO file that contains phenotype data {PINK}", 
+        type = str,
+        required = True
+    )
     args = parser.parse_args()
 
     if not os.path.isfile(args.geno) or not os.path.isfile(args.pheno):
