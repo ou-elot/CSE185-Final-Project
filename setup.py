83be41ecd641e76ba8eae189b1b70d212e25c225
ou-elot <146787722+ou-elot@users.noreply.github.com>
2024-06-02 00:02:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     name='mygwas',
     version=VERSION,
     description='CSE185 Final GWAS',
-    author='Elliott Ou, Lenny Lei',
-    author_email='elou@ucsd.edu, wlei@ucsd.edu',
+    author='Elliott Ou, Lenny Lei, Audria Montalvo',
+    author_email='elou@ucsd.edu, wlei@ucsd.edu, ansaravi@ucsd.edu',
     packages=find_packages(),
     entry_points={
         "console_scripts": [
