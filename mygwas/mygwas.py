0ec23273d86597ff3f5987b6eb85f097bd95b490
ou-elot <146787722+ou-elot@users.noreply.github.com>
2024-06-03 14:40:30 -0700

    Update mygwas.py

--- mygwas/mygwas.py
+++ mygwas/mygwas.py
@@ -39,7 +39,7 @@ def main():
             "------------------------------------------------------------\n"
             f"Example usage:\n"
             f"{BLUE}mygwas --geno path/to/genotype.vcf --pheno path/to/phenotype.phen {PINK}\n"
-            "============================================================ {WHITE}"
+            "============================================================ {WHITE} "
         ),
         formatter_class=argparse.RawTextHelpFormatter
     )
